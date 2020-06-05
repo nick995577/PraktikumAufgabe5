da64fcaff4d25e00ec2f6862dc3b4d0abb8579cb
Niklas Koch <niklas.koch@hs-furtwangen.de>
2020-06-05 08:55:58 +0200

    new files

--- src/main/java/de/hfu/HauptKlasse.java
+++ src/main/java/de/hfu/HauptKlasse.java
@@ -3,12 +3,21 @@ package de.hfu;
 import java.util.Scanner;
 
 public class HauptKlasse{
+	/**
+	 * Das ist die Hauptklasse, darin enthalten ist die Main Methode.
+	 */
 	public static void main(String[] args){
+		/**
+		 * Anfang der Main
+		 */
 		Scanner scanner = new Scanner(System.in);
 		System.out.print("Enter a string: ");
 		String s1 = scanner.nextLine();
 		 
 		System.out.print(s1.toUpperCase());
+		/**
+		 * Ende der Main
+		 */
 	}
 
 }
